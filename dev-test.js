e559cde9a7c5b2bde6aa1a83d152df24b9598b7e
David Katz <15Dkatz@shcp.edu>
2018-02-05 22:49:02 -0800

    Test blockchain

--- dev-test.js
+++ dev-test.js
@@ -1,4 +1,4 @@
 const Block = require('./block');
 
-const block = new Block('foo', 'bar', 'zoo', 'baz');
-console.log(block.toString());
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
