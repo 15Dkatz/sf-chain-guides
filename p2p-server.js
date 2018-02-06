dc6381f8a40ba77a3d53cde4116496647d1c8eb1
David Katz <15Dkatz@shcp.edu>
2018-02-06 09:59:12 -0800

    Listen with the P2PServer

---
+++ p2p-server.js
@@ -0,0 +1,21 @@
+const Websocket = require('ws');
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+class P2pServer {
+  constructor(blockchain) {
+    this.blockchain = blockchain;
+    this.sockets = [];
+  }
+
+  listen() {
+    const server = new WebSocket.Server({ port: P2P_PORT });
+    server.on('connection', socket => this.connectSocket(socket));
+  }
+
+  connectSocket(socket) {
+    this.socket.push(socket);
+    console.log('Socket connected');
+  }
+}
