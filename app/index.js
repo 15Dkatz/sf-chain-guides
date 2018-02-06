a7feae7294739940209bec6d94078dcb73b8dd36
David Katz <15Dkatz@shcp.edu>
2018-02-06 09:37:21 -0800

    POST mine block

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -6,8 +7,17 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/blocks', (req, res) => {
   res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+  const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+
+  res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
