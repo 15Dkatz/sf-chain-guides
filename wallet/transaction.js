2ca287aa7067bf1aae8affc13ad81483c6110556
David Katz <15Dkatz@shcp.edu>
2018-02-17 00:48:57 -0800

    Update transactions and test updates

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -7,6 +7,21 @@ class Transaction {
     this.outputs = [];
   }
 
+  update(senderWallet, recipient, amount) {
+    const senderOutput = this.outputs.find(output => output.address === senderWallet.publicKey);
+
+    if (amount > senderOutput.amount) {
+      console.log(`Amount: ${amount} exceeds balance.`);
+      return;
+    }
+
+    senderOutput.amount = senderOutput.amount - amount;
+    this.outputs.push({ amount, address: recipient });
+    Transaction.signTransaction(this, senderWallet);
+
+    return this;
+  }
+
   static newTransaction(senderWallet, recipient, amount) {
     const transaction = new this();
 
--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -44,4 +44,24 @@ describe('Transaction', () => {
       expect(transaction).toEqual(undefined);
     });
   });
+
+  describe('and updating a transaction', () => {
+    let nextAmount, nextRecipient;
+
+    beforeEach(() => {
+      nextAmount = 20;
+      nextRecipient = 'n3xt-4ddr355';
+      transaction = transaction.update(wallet, nextRecipient, nextAmount);
+    });
+
+    it(`subtracts the next amount from the sender's output`, () => {
+      expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+        .toEqual(wallet.balance - amount - nextAmount);
+    });
+
+    it('outputs an amount for the next recipient', () => {
+      expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
+        .toEqual(nextAmount);
+    });
+  });
 });
